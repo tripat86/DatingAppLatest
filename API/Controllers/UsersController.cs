ad43b7e0460d6c1c1c921b2b0a02c7737f20d288
Tripat <tripatsimransingh@gmail.com>
2021-11-18 22:09:05 +0530

    Added .Net6 API Project

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetAllUser()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
